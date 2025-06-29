dd5628aa13bd4265463b21d58800ba891bb12c79
Peter Mai <peter.mai1996@gmail.com>
2025-06-29 11:12:48 -0500

    Created counting variable in order to count number of guesses for player

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 document.querySelector('#submit').addEventListener('click', checkGuess )
 
+let count = 0;
+
 function checkGuess(){
     // Storing player Guess
     const playerGuess = document.querySelector('#guess').value;
@@ -9,15 +11,18 @@ function checkGuess(){
     const randomNumber = Math.floor(Math.random() * 101)
 
     if(playerGuess < randomNumber){
+        count++;
         document.querySelector('#tooHighOrLow').innerHTML = 'Too low'
     }
 
      else if (playerGuess > randomNumber){
+        count++;
         document.querySelector('#tooHighOrLow').innerHTML = 'Too High'
 
     }
 
     else {
+        count++;
         document.querySelector('#tooHighOrLow').innerHTML = 'You guessed correctly!'
     }
 }
